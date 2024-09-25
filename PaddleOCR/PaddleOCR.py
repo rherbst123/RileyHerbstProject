510f1ccec0ce643389ff8ba6c9ac3fdf82bb90a0
Riley Herbst <rileyherbst@yahoo.com>
2024-09-25 15:24:51 -0500

    now working

--- PaddleOCR/PaddleOCR.py
+++ PaddleOCR/PaddleOCR.py
@@ -3,7 +3,7 @@ from paddleocr import PaddleOCR,draw_ocr
 # You can set the parameter `lang` as `ch`, `en`, `fr`, `german`, `korean`, `japan`
 # to switch the language model in order.
 ocr = PaddleOCR(use_angle_cls=True, lang='en') # need to run only once to download and load model into memory
-img_path = 'c:\\Users\\Riley\\Desktop\\V0577013F.jpg'
+img_path = 'c:\\Users\\Riley\\Documents\\Github\\RileyHerbstProject\\PaddleOCR\\V0577013F.jpg'
 result = ocr.ocr(img_path, cls=True)
 for idx in range(len(result)):
     res = result[idx]
@@ -20,4 +20,4 @@ txts = [line[1][0] for line in result]
 scores = [line[1][1] for line in result]
 im_show = draw_ocr(image, boxes, txts, scores, font_path='C:/Windows/Fonts/Arial.ttf')
 im_show = Image.fromarray(im_show)
-im_show.save('result2.jpg')
+im_show.save('result3.jpg')
